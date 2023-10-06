8b12ce291a6f6f70b15101c01a82a426926f9693
segni <segniwoldemichael@gmail.com>
2023-10-06 10:05:23 -0700

    C program that prints Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - This is the entry point
+ * Return: This function returns 0 when compiled succesfully
+ */
+int main(void)
+{
+	/*This block of code print the following to the output console*/
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
