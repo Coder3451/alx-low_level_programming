2eb2990458d0e2a8f47d72684ac9b95dde49112f
segni <segniwoldemichael@gmail.com>
2023-10-07 02:42:09 -0700

    C program that print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - it prints the message, followed by a new line , to the standard error
+ * Return: it returns 1 when successfully compiled
+ */
+int main(void)
+{
+	const char *str = "and that piece of art is useful\" - Dora korpar, "
+			  "2025-10-19\n";
+	size_t len = strlen(str);
+
+	write(STDERR_FILENO, str, len);
+	return (1);
+}
