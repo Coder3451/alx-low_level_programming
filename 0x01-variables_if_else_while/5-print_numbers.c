e62ddb63103e93fd5432577d1f863df0e537d0d8
segni <segniwoldemichael@gmail.com>
2023-10-09 05:46:17 -0700

    Prints all single digit numbers of base 10 starting from 0, followed by a new line

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: it returns 0 when successfully executed
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num ++)
+	{
+		fprintf(stdout, "%d", num);
+	}
+	fprintf(stdout, "\n");
+        return (0);
+}
