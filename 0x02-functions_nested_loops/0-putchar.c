577ccff1f4b635f3afe2383787437f40758fa72b
segni <segniwoldemichael@gmail.com>
2023-10-11 13:36:24 -0700

    Print _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,11 @@
  * main - entry point
  * Return: return 0 when successfully executed
  */
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}
-
 int main(void)
 {
 	char print[] = "_putchar\n";
 	int i = 0;
+
 	while (print[i] != '\0')
 	{
 		_putchar(print[i]);
