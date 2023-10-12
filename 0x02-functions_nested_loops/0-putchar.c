95e3f6a6b63367da78e6be71fc0fc2587483edc5
segni <segniwoldemichael@gmail.com>
2023-10-12 00:28:10 -0700

    Print "_putchar", followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "main.h"
+/**
+ * _putchar - it print character
+ * @c: takes one character parameter
+ *
+ * Return: return charcter c
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 /**
  * main - entry point
  * Return: return 0 when successfully executed
  */
 int main(void)
 {
-	char print[] = "_putchar\n";
+	char display[] = "_putchar\n";
 	int i = 0;
 
-	while (print[i] != '\0')
+	while (display[i] != '\0')
+
 	{
-		_putchar(print[i]);
+		_putchar(display[i]);
 		i++;
 	}
 	return (0);
