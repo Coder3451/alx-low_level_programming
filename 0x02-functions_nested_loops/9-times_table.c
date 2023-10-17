d591c42699814fbd27dafb9d52b5212328dcbc5b
segni <segniwoldemichael@gmail.com>
2023-10-17 04:27:26 -0700

    Prints the 9 times table, starting with 0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - Prints the 9 times table, starting with 0
+ *
+ * Return: Always 0.
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int k;
+			int firstDigit;
+			int lastDigit;
+
+			k = i * j;
+			firstDigit = k / 10;
+			lastDigit = k % 10;
+			if (k >= 10)
+			{
+				_putchar('0' + firstDigit);
+				_putchar('0' + lastDigit);
+			}
+			else
+			{
+				_putchar('0' + k);
+			}
+
+			if (j != 9)
+			{
+				_putchar(44);
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
