84270d24a4fc4d81cee65b800846fc7dbf9cd975
segni <segniwoldemichael@gmail.com>
2023-10-18 01:36:11 -0700

    Prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,40 +7,34 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, product;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			int k;
-			int firstDigit;
-			int lastDigit;
-
-			k = i * j;
-			firstDigit = k / 10;
-			lastDigit = k % 10;
-			if (k >= 10)
+			product = i * j;
+			if (product < 10)
 			{
-				_putchar('0' + firstDigit);
-				_putchar('0' + lastDigit);
+				if (j != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(product + '0');
 				if (j != 9)
 				{
 					_putchar(44);
 					_putchar(' ');
-					if (j >= 2 && i <= 4)
-						_putchar(' ');
 				}
 			}
 			else
 			{
-				_putchar('0' + k);
+				_putchar((product) / 10 + '0');
+				_putchar((product) % 10 + '0');
 				if (j != 9)
 				{
 					_putchar(44);
 					_putchar(' ');
-					if (j >= 2 && i <= 4)
-						_putchar(' ');
 				}
 			}
 		}
