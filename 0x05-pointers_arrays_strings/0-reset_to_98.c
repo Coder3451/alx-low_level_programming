a8add2b322e48f718c6c2e0054905e442afece4e
segni <segniwoldemichael@gmail.com>
2023-10-20 23:08:10 -0700

    Takes a pointer to an int as parameter and updates the value it points to to 98.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * @*n: integet pointer parameter
+ * @n: integet pointer parameter
  */
 void reset_to_98(int *n)
 {
