49b1b028444614a46455643b4c12531f092f91e4
segni <segniwoldemichael@gmail.com>
2023-10-22 12:35:38 -0700

    Task 10

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a[5] = {1, 2, 3, 4, 5};
+	int *p;
+
+	p = &a[2];
+	*(p + 1) = 98; /* Added line */
+
+	printf("a[2] = %d\n", a[2]);
+
+	return (0);
+}
