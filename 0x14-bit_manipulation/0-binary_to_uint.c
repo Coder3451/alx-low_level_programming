864d76fa1a4c7ef6eababdc85204f6466d8c524d
Coder3451 <segniwoldemichael@gmail.com>
2025-02-06 13:44:50 -0800

    Include the strlen function together with the binary to uint fuction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * _strlen - calculate the length of a string
+ * @s: the string
+ *
+ * Return: return the length.
+ */
+size_t _strlen(const char *s)
+{
+        size_t length = 0;
+
+        if (s == NULL)
+        {
+                return (0);
+        }
+
+        while (s[length] != '\0')
+        {
+                length++;
+        }
+
+        return (length);
+}
+
+/**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: points to a string of 0 and 1 chars
  *
