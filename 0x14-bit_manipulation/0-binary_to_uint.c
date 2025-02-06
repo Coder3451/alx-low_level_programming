eeb7afa7eb285bee56b347a12381de84c24ef255
Coder3451 <segniwoldemichael@gmail.com>
2025-02-06 13:51:54 -0800

    Update the kernel style of 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,17 @@
 size_t _strlen(const char *s)
 {
 	size_t length = 0;
-	
+
 	if (s == NULL)
 	{
 		return (0);
 	}
-	
+
 	while (s[length] != '\0')
 	{
 		length++;
 	}
-	
+
 	return (length);
 }
 
