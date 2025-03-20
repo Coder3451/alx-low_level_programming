1e7e3745feb70dfa84f7d7c199b5ed6d4a11aaea
Coder3451 <segniwoldemichael@gmail.com>
2025-03-20 21:45:23 +0300

    Updating the Variadic funcitons and Functions Pointers tasks

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name.
+ * @name: name.
+ * @f: pointer.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: String to be printed between the strings.
+ * @n: the number of strings passed to the function.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+}
