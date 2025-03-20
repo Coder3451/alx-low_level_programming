36b0f194736ecf6decba8e89c04445991186cbf1
Coder3451 <segniwoldemichael@gmail.com>
2025-03-20 23:26:56 +0300

    Update print all to meet requirement

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,53 +7,40 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i;
-	char c;
+	int i, count;
+	char c, *s;
 	double f;
-	char *s;
-	int count;
 	const char *sep;
 
 	va_start(args, format);
 	count = 0;
 	sep = "";
-
 	while (format && format[count])
 	{
 		switch (format[count])
 		{
-			case 'c':
-			{
-				c = (char)va_arg(args, int);
-				printf("%s%c", sep, c);
-				sep = ", ";
-				break;
-			}
-			case 'i':
-			{
-				i = va_arg(args, int);
-				printf("%s%d", sep, i);
-				sep = ", ";
-				break;
-			}
-			case 'f':
-			{
-				f = va_arg(args, double);
-				printf("%s%f", sep, f);
-				sep = ", ";
-				break;
-			}
-			case 's':
-			{
-				s = va_arg(args, char *);
-				if (!s)
-					s = "(nil)";
-				printf("%s%s", sep, s);
-				sep = ", ";
-				break;
-			}
-			default:
-				break;
+		case 'c':
+			c = (char)va_arg(args, int);
+			printf("%s%c", sep, c);
+			sep = ", ";
+			break;
+		case 'i':
+			i = va_arg(args, int);
+			printf("%s%d", sep, i);
+			sep = ", ";
+			break;
+		case 'f':
+			f = va_arg(args, double);
+			printf("%s%f", sep, f);
+			sep = ", ";
+			break;
+		case 's':
+			s = va_arg(args, char *);
+			if (!s)
+				s = "(nil)";
+			printf("%s%s", sep, s);
+			sep = ", ";
+			break;
 		}
 		count++;
 	}
